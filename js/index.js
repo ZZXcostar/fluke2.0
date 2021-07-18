c94a8f63cbf9ad57edb8a24fc86aa6e0c492d8e8
ZZXcostar <zouzouxi@foxmail.com>
2021-07-18 11:21:59 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,4 +1,33 @@
     function init(isPCFlag){
+        
+        
+        //     // 背景图动画
+        function backgroundAnimation(){
+            var pageBody = $('#pageBody');
+            var x = 20;
+            var flag = true;
+            
+            function polling() {
+                if(flag){
+                    x -= 10;
+                    pageBody.animate({
+                        'background-position-x': x+'%',
+                    }, 2000, 'linear');
+                    flag = false
+                // changeImage()
+                }else{
+                    x += 10;
+                    pageBody.animate({
+                        'background-position-x': x+'%',
+                    }, 2000, 'linear');
+                    flag = true
+                    //changeImage()
+                }
+                setTimeout(polling,2000)
+            }
+            polling();
+        }
+        // backgroundAnimation()
         $(".meritBox li").mouseover(function(){
             $(".contentText .imgText").hide();
             // $(".meritBox .imgbox").css({top:"0px"});
@@ -135,157 +164,149 @@
                 left:"-116%",
             },2000,'swing',goamian)
         }
-        
-    //     // 背景图动画
-        function backgroundAnimation(){
-            var pageBody = $('#pageBody');
-            var x = 10;
-            var flag = true;
-            var currentIndex = 0;
-            var bgImgs = ["./img/背景-7.jpg","./img/背景-6.jpg","./img/背景-5.jpg","./img/背景-4.jpg","./img/背景-3.jpg","./img/背景-2.jpg","./../img/背景-1.jpg"];
-            function polling() {
-                if(flag){
-                    x -= 10;
-                    pageBody.animate({
-                        'background-position-x': x+'%',
-                    }, 2000, 'linear');
-                    flag = false
-                   // changeImage()
-                }else{
-                    x += 10;
-                    pageBody.animate({
-                        'background-position-x': x+'%',
-                    }, 2000, 'linear');
-                    flag = true
-                    //changeImage()
-                }
-                setTimeout(polling,2000)
-            }
-            polling();
-            // function changeImage() {
-            //     if (currentIndex > bgImgs.length){
-            //         currentIndex = 0;
-            //     }else{
-            //         pageBody.css('background-image', 'url(' + bgImgs[currentIndex] + ')');
-            //         currentIndex += 1;
-            //     }
-            // }
-            // var currentIndex = 0;
-            // function changeImage(){
-                
-            //     var bgImgs = ["voice-right-1.png","voice-right-2.png","voice-right-3.png"];
-            //     if (currentIndex >= bgImgs.length){
-            //       currentIndex = 0;
-            //     }else{
-            //        $('.cover_right').css('background-image', 'url(' + bgImgs[currentIndex] + ')');
-            //        currentIndex += 1;
-            //     }
-            // }
-            // setInterval(changeImage ,200)
-        }
-        backgroundAnimation()
 
         // 三角光点 光线动画
         function goamian(){
             $(".triangle").fadeIn()
-            let css = {left:'83%',opacity: '0'};
-            let interval = null
-            clearInterval(interval)
-            interval = setInterval(function(){ // 这里要改成回调 定时器有性能问题
-                $(".lineImg1").addClass("intro");
-                $(".lineImg2").addClass("intro");
-                $(".lineImg3").addClass("intro");
-    
-                $('.lineImg1').animate(css,1000,rowBack1);
-                $('.lineImg2').animate(css,1000,rowBack2);
-                $('.lineImg3').animate(css,1000,rowBack3);
-            },0);
-            function rowBack1(){
-                if(css.left==='83%'){
-                    $('.lineImg1').css('left', '0%')
-                    $(".lineImg1").removeClass("intro");
-                    // $('.lineImg1').fadeOut()
-                }
-            }
-            function rowBack2(){
-                if(css.left==='83%'){
-                    $('.lineImg2').css('left', '0%')
-                    $(".lineImg2").removeClass("intro");
-                    // $('.lineImg2').fadeOut()
-                }
-            }
-            function rowBack3(){
-                if(css.left==='83%'){
-                    $('.lineImg3').css('left', '0%')
-                    $(".lineImg3").removeClass("intro");
-                    // $('.lineImg3').fadeOut()
-                }
-            }
-            $('.lineImg1').animate({top: '-14px',height: '24px'},0)
-            $('.lineImg2').animate({top: '-14px',height: '24px'},0)
-            $('.lineImg3').animate({top: '-14px',height: '24px'},0)
-            $(".ocvBox .ocvTitle").animate({
-                fontSize: '12px'
-            },1000);
-            $(".arcBox .arcTitle").animate({
-                fontSize: '12px'
-            },1000);
-            $(".switchBox .switchTitle").animate({
-                fontSize: '12px'
-            },1000);
-            $('.secondAnimation').css('position','absolute')
-            if(isPCFlag){
-                $('.secondAnimation').animate({left: '45%'},1000)
-            }else{
-                $('.secondAnimation').animate({left: '47.7%',top: '47%'},1000)
-            }
-            $('.triangle_content').animate({width: '100px',height: '70px'},1000,showLight);
-            
-            function showLight(){
-                $('.secondAnimation').fadeOut()
-                $('.thirdAnimation').fadeIn()
-                setTimeout(()=>{
-                  $('.thirdAnimation').fadeOut(0, showProduct)
-                },500)
-            }
-            function showProduct(){
-                $('.fourthAnimation').fadeIn()
-                $('.fourthAnimation').css('display','flex')
-                if(isPCFlag){
-                    $('.product').animate({width: '580px',height: '580px'},1000)
-                }else{
-                    $('.product').animate({width: '3rem',height: '3rem'},1000)
-                }
-                $('.productImg1').animate({width: '100%',height: '100%',top: '46px',left: '0%'},1000, hideLight)
-            }
-            function hideLight(){
-                $('.lightImg2').fadeOut()
-                $('.productImg1').css('background-image',"url('./img/8-产品1（无光源+倒影）.png')")
-                // $('.productImg1').fadeOut(100,()=>{
-                //     $('.productImg2').fadeIn()
-                // })
-                $('.productImg2').fadeIn(100,showProductDesc)
 
+
+            $(".lineRight").fadeIn()
+            $(".lineImg3").addClass("intro");
+            $('.lineImg3').animate({left:'83%',opacity: '0'},1000,setblack1);
+            setTimeout(() => {
+                $(".lineTop").fadeIn()
+                $(".lineImg1").addClass("intro");
+                $('.lineImg1').animate({left:'83%',opacity: '0'},1000,setblack2);
+                $('.lineImg1').css('left', '0%')
+            }, 1000);
+            setTimeout(() => {
+                $(".lineLeft").fadeIn()
+                $(".lineImg2").addClass("intro");
+                $('.lineImg2').animate({left:'83%',opacity: '0'},1000,setblack3);
+                $('.lineImg2').css('left', '0%')
+            }, 2000);
+        
+            function setblack1(){
+                $('.lineImg3').css('left', '0%')
+                $('.lineImg3').removeClass('intro')
             }
-           function showProductDesc(){
-               
-                $('.product_content').animate({
-                    'width': '50%',
-                }, 1000, 'linear');
-                $('.product_desc').animate({
-                    'width': '50%',
-                }, 1000, 'linear',()=>{
+            function setblack2(){
+                $('.lineImg1').css('left', '0%')
+                $('.lineImg1').removeClass('intro')
+            }
+            function setblack3(){
+                $('.lineImg2').css('left', '0%')
+                $('.lineImg2').removeClass('intro')
+            }
+
+            setTimeout(() => {
+                let css = {left:'83%',opacity: '0'};
+                let interval = null
+                clearInterval(interval)
+                interval = setInterval(function(){ // 这里要改成回调 定时器有性能问题
+                    $(".lineImg1").addClass("intro");
+                    $(".lineImg2").addClass("intro");
+                    $(".lineImg3").addClass("intro");
+        
+                    $('.lineImg1').animate(css,1000,rowBack1);
+                    $('.lineImg2').animate(css,1000,rowBack2);
+                    $('.lineImg3').animate(css,1000,rowBack3);
+                },0);
+                function rowBack1(){
+                    if(css.left==='83%'){
+                        $('.lineImg1').css('left', '0%')
+                        $(".lineImg1").removeClass("intro");
+                        // $('.lineImg1').fadeOut()
+                    }
+                }
+                function rowBack2(){
+                    if(css.left==='83%'){
+                        $('.lineImg2').css('left', '0%')
+                        $(".lineImg2").removeClass("intro");
+                        // $('.lineImg2').fadeOut()
+                    }
+                }
+                function rowBack3(){
+                    if(css.left==='83%'){
+                        $('.lineImg3').css('left', '0%')
+                        $(".lineImg3").removeClass("intro");
+                        // $('.lineImg3').fadeOut()
+                    }
+                }
+                $('.lineImg1').animate({top: '-14px',height: '24px'},0)
+                $('.lineImg2').animate({top: '-14px',height: '24px'},0)
+                $('.lineImg3').animate({top: '-14px',height: '24px'},0)
+                $(".ocvBox .ocvTitle").animate({
+                    fontSize: '12px'
+                },1000);
+                $(".arcBox .arcTitle").animate({
+                    fontSize: '12px'
+                },1000);
+                $(".switchBox .switchTitle").animate({
+                    fontSize: '12px'
+                },1000);
+
+
+                $('.secondAnimation').css('position','absolute')
+                if(isPCFlag){
+                    $('.secondAnimation').animate({left: '45%'},1000)
+                }else{
+                    $('.secondAnimation').animate({left: '47.7%',top: '47%'},1000)
+                }
+                $('.triangle_content').animate({width: '100px',height: '70px'},1000,showLight);
+                
+                function showLight(){
+                    $('.secondAnimation').fadeOut()
+                    $('.thirdAnimation').fadeIn()
+                    setTimeout(()=>{
+                    $('.thirdAnimation').fadeOut(0, showProduct)
+                    },500)
+                }
+                function showProduct(){
+                    $('.fourthAnimation').fadeIn()
+                    $('.fourthAnimation').css('display','flex')
+                    if(isPCFlag){
+                        $('.product').animate({width: '2.7rem',height: '2.7rem'},1000)
+                    }else{
+                        $('.product').animate({width: '3rem',height: '3rem'},1000)
+                    }
+                    $('.productImg1').animate({width: '100%',height: '100%',top: '46px',left: '0%'},1000, hideLight)
+                }
+                function hideLight(){
+                    $('.lightImg2').fadeOut()
+                    $('.productImg1').css('background-image',"url('./img/8-产品1（无光源+倒影）.png')")
+                    // $('.productImg1').fadeOut(100,()=>{
+                    //     $('.productImg2').fadeIn()
+                    // })
+                    $('.productImg2').fadeIn(100,showProductDesc)
+
+                }
+            function showProductDesc(){
+                
+                    $('.product_content').animate({
+                        'width': '50%',
+                    }, 1000, 'linear');
                     $('.product_desc').animate({
-                        'opacity': '1'
+                        'width': '50%',
                     }, 1000, 'linear',()=>{
-                        // setTimeout(()=>{
-                            $(".contentText .imgText").eq(0).fadeIn(1000)
-                            $(".setbutton").fadeIn(1000)
-                            $(".dilaog").hide()
-                        // },00)
+                        $('.title_img').animate({
+                            'opacity': '1'
+                        }, 1000, 'linear',()=>{
+                            $('.dscbottomBox').animate({
+                                'opacity': '1'
+                            }, 1000, 'linear',()=>{
+                                // setTimeout(()=>{
+                                    $(".contentText .imgText").eq(0).fadeIn(1000)
+                                    $(".setbutton").fadeIn(1000)
+                                    $(".dilaog").hide()
+                                // },00)
+                            })
+                        });
                     });
-                });
-            }
+                }
+
+            }, 3500);
+            
         }
         // goamian()
 
